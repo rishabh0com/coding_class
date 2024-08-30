ed4dffcd7f6dedbcbc0bb158a1345798e461a513
Rishabh Tripathi <altrodav@gmail.com>
2024-08-30 21:57:31 +0530

    added slice.js

---
+++ array_method/slice.js
@@ -0,0 +1,12 @@
+/*
+JavaScript Array slice()
+The slice() method slices out a piece of an array into a new array:
+Note
+The slice() method creates a new array.
+
+The slice() method does not remove any elements from the source array.
+ */
+
+const fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
+const citrus = fruits.slice(2);
+console.log(citrus)
