9df51fc92f905ab4c564926c1fa9411d8c252661
Rishabh Tripathi <altrodav@gmail.com>
2024-09-02 22:08:28 +0530

    added print odd program

---
+++ for_loop/sept_02_24/print_odd.js
@@ -0,0 +1,13 @@
+// Print Odd Numbers from 1 to 10
+// Question: Write a for loop that prints all the odd numbers between 1 and 10.
+
+// num % 2 == 0 -> even
+
+for (let i = 1; i <= 10; i++) {
+  // if (i % 2 != 0) {
+  //                   //
+  // }
+  if (i % 2 == 1) {
+    console.log(i);
+  }
+}
