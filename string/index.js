01f144fb4e59c4a15c69810f42df68d79b23ec94
Rishabh Tripathi <altrodav@gmail.com>
2024-09-17 22:42:19 +0530

    added string and their methods

---
+++ string/index.js
@@ -0,0 +1,36 @@
+// string
+let naam = "John Doe";
+// console.log(naam[0]) // naam.chatAt(0)
+
+// string methods
+// 1=> .toUpperCase();
+// console.log(naam.toUpperCase());
+//2. => .toLowerCase
+// console.log(naam.toLowerCase());
+
+// 3=> .includes();
+// console.log(naam.includes("D"));
+// console.log(naam.includes("Deo"));
+
+//4 => .indexOf();
+// console.log(naam.indexOf("Doe")); // 5
+
+// 5 => .subString(start index, end index);
+// console.log(naam.substring(1,6));
+
+// 6=> .slice
+// const slicedString = naam.slice(0, 6);
+// console.log(slicedString);
+// console.log(naam.slice(-5))
+// console.log(naam.slice(5))
+
+// const str = "    Rishabh Tripathi   ";
+// console.log(str,"space:",str.trim())
+
+const fruits = "apple,mango,banana";
+const fruitsArr = fruits.split(",")
+console.log(fruits,fruitsArr)
+
+
+
+ 
