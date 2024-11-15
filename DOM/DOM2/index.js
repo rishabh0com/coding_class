c8a2cbf6025e9e94921cad9b836309d5c3ae6c1e
Rishabh Tripathi <rishabhtripathi587@gmail.com>
2024-11-15 22:52:34 +0530

    dom

---
+++ DOM/DOM2/index.js
@@ -0,0 +1,11 @@
+const resPro = fetch("https://fakestoreapi.com/products/");
+resPro
+  .then((res) => {
+    return res.json();
+  })
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
--- DOM/calculator/index.js
+++ DOM/calculator/index.js
@@ -2,7 +2,7 @@ const allBtns = document.querySelectorAll(".btn");
 const equal = document.getElementById("equal");
 const operators = document.querySelectorAll(".operator");
 const input = document.getElementById("inp");
-console.log(allBtns, equal, operators);
+// console.log(allBtns, equal, operators);
 
 allBtns.forEach((element) => {
   element.addEventListener("click", () => {
@@ -11,7 +11,7 @@ allBtns.forEach((element) => {
 });
 operators.forEach((ope) => {
   ope.addEventListener("click", () => {
-    input.value += ope.innerText;
+    input.value += ope.name;
   });
 });
 
@@ -19,3 +19,18 @@ equal.addEventListener("click", () => {
   const answer = eval(input.value);
   input.value = answer;
 });
+
+// function for All Clear Button
+document.getElementById("AC").addEventListener("click", () => {
+  input.value = "";
+  // console.log("clear");
+});
+
+document.getElementById("clear").addEventListener("click", () => {
+  // console.log(input.value);
+  input.value = input.value.slice(0, -1);
+});
+
+
+
+slice(0,2)
